ce998d6f4535c69ef89cae153c1f6b6804dab719
Yuri Silva <yurirssilva@gmail.com>
2025-07-24 19:20:52 -0300

    chore: number

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ function formatarData(date) {
 
 wppconnect
   .create({
-    phoneNumber: '5577991829075',
+    phoneNumber: '5577991008357',
     catchLinkCode: (str) => console.log('Code: ' + str),
     browserArgs: ['--no-sandbox']
   })
